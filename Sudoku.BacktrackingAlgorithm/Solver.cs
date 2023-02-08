485709152ef35a1aff389d166bf2d5527d5e0007
Juliette <juliette.crespo@epfedu.fr>
2023-02-08 11:25:31 +0100

    Creation solver

--- Sudoku.BacktrackingAlgorithm/Solver.cs
+++ Sudoku.BacktrackingAlgorithm/Solver.cs
@@ -2,4 +2,11 @@ namespace Sudoku.BacktrackingAlgorithm;
 //test
 public class Solver
 {
+    public class GeneticEmptySolver : ISudokuSolver
+	{
+		public SudokuGrid Solve(SudokuGrid s)
+		{
+			return s.CloneSudoku();
+		}
+	}
 }
