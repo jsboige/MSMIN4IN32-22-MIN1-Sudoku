9f0e8a795a9fe02e34266ab65e3db5e8668c92a6
marvinbidzana@gmail.com <marvinbidzana@gmail.com>
2023-02-08 12:03:00 +0100

    Code débugué

---
+++ Sudoku.Csp/SudokuSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.Csp;
+
+public class SudokuSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+}
