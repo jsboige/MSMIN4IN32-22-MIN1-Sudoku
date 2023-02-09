ce7c9cdf15df7d864290280884b22e95e62183e1
Louis <louis.carozzo@gmail.com>
2023-02-09 15:47:04 +0100

    Reset

--- Sudoku.PSO/PSOSolver.cs
+++ Sudoku.PSO/PSOSolver.cs
@@ -7,29 +7,11 @@ namespace Sudoku.PSO;
 public class PSOSolver : ISudokuSolver
 {
         public SudokuGrid Solve(SudokuGrid s)
-        {
-            // Console.WriteLine("Begin solving Sudoku using combinatorial evolution");
-            // Console.WriteLine("The Sudoku is:");
-
-            // var sudoku = Sudoku.Easy;
-            // Console.WriteLine(sudoku.ToString());
-
-            // const int numOrganisms = 200;
-            // const int maxEpochs = 5000;
-            // const int maxRestarts = 40;
-            // Console.WriteLine($"Setting numOrganisms: {numOrganisms}");
-            // Console.WriteLine($"Setting maxEpochs: {maxEpochs}");
-            // Console.WriteLine($"Setting maxRestarts: {maxRestarts}");
-
-            // var solver = new SudokuSolver();
-            // var solvedSudoku = solver.Solve(sudoku, numOrganisms, maxEpochs, maxRestarts);
-
-            // Console.WriteLine("Best solution found:");
-            // Console.WriteLine(solvedSudoku.ToString());
-            // Console.WriteLine(solvedSudoku.Error == 0 ? "Success" : "Did not find optimal solution");
-            // Console.WriteLine("End Sudoku using combinatorial evolution");
-            Console.WriteLine("Sudoku est comme :");
-            Console.WriteLine(s.CloneSudoku());
+        {   
+            //s.Cells --> code pso
+            //resolution pso
+            //s.Cells = recup tableau
+            //return s;
             return s.CloneSudoku();
         }
     }
