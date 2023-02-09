07509c8ab5d84dd31f0789a96b003465a2f9b005
Louis <louis.carozzo@gmail.com>
2023-02-09 16:01:44 +0100

    trying to get sudoku

--- Sudoku.PSO/PSOSolver.cs
+++ Sudoku.PSO/PSOSolver.cs
@@ -8,7 +8,15 @@ public class PSOSolver : ISudokuSolver
 {
         public SudokuGrid Solve(SudokuGrid s)
         {   
+            int[,] output = new int[9,9];
             //s.Cells --> code pso
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    output[i,j] = s.Cells[i][j];
+                }
+            }
+            Console.Write("Sudoku be like :");
+            Console.Write(output.ToString());
             //resolution pso
             //s.Cells = recup tableau
             //return s;
