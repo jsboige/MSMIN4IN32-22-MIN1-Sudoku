229dae15edc3f7dc05ee72a105c971a5a4aac79e
Nicolas <ni.coulomb@icloud.com>
2023-02-16 19:12:02 +0100

    Code cleaning

--- ColorationGraphes/ColoredGraphSolver.cs
+++ ColorationGraphes/ColoredGraphSolver.cs
@@ -12,174 +12,14 @@ public class ColoredGraphSolver:ISudokuSolver
     {
         return ((index - index % 9) / 9, index % 9);
     }
-    private SudokuGrid FirstSolver(SudokuGrid s)
-    {
-        ColoringAlgorithm colorAlgo = new ColoringAlgorithm(81);
-        int longTab = 9;
-
-        for (int i = 0; i < longTab; i++)
-        {
-            for (int j = 0; j < longTab; j++)
-            {
-                colorAlgo.InitColoring(ConvertIndex((i, j)), s.Cells[i][j]);
-                
-                foreach ((int row, int column) neighbours in SudokuGrid.CellNeighbours[i][j])
-                {
-                    colorAlgo.AddEdge(ConvertIndex((i, j)), ConvertIndex(neighbours));
-                }
-            }
-        }
-        
-        string sudokuSolved = colorAlgo.GreedyColoring();
-        SudokuGrid sSolved = SudokuGrid.ReadSudoku(sudokuSolved);
-
-        return sSolved;
-    }
-
-    private SudokuGrid SecondSolver(SudokuGrid s)
+    public SudokuGrid Solve(SudokuGrid s)
     {
         DSatur dSatur = new DSatur(s);
         string sudokuSolved = dSatur.initDataMatrix();
         SudokuGrid sSolved = SudokuGrid.ReadSudoku(sudokuSolved);
-
-        return sSolved;
-    }
-    
-    public SudokuGrid Solve(SudokuGrid s)
-    {
-        //SudokuGrid sSolved = FirstSolver(s);
-        SudokuGrid sSolved = SecondSolver(s);
         return sSolved;
     }
 }
-
- public class ColoringAlgorithm
-{
-    private int V; // No. of vertices
-    private List<List<int>> adj;
-    private int[] result;
-    
-    
-    //Constructor
-    public ColoringAlgorithm(int v)
-    {
-        V = v;
-        result = new int[V];
-        adj = new List<List<int>>();
-        for (int i = 0; i < v; ++i)
-        {
-            adj.Add(new List<int>());
-        }
-    }
-
-    //Function to add an edge into the graph
-    public void AddEdge(int v, int w)
-    {
-        adj[v].Add(w);
-        adj[w].Add(v); //Graph is undirected
-    }
-
-    // Assigns colors (starting from 0) to all vertices and
-    // prints the assignment of colors
-
-    public void InitColoring(int index, int value)
-    {
-        result[index] = value - 1;
-    }
-    public string GreedyColoring()
-    {
-        // A temporary array to store the available colors. True
-        // value of available[cr] would mean that the color cr is
-        // assigned to one of its adjacent vertices
-        int boucle = 0;
-        while(boucle < 81 && Array.IndexOf(result, -1) != -1){
-            bool[] available = StoreColors();
-
-            // Assign colors to remaining V-1 vertices
-            AsignColors(available);
-            boucle++;
-        }
-        
-        /*
-        List<ResponseModel> responseList = new List<ResponseModel>();
-        // print the result
-        for (int u = 0; u < V; u++)
-        {
-            responseList.Add(new ResponseModel(u, result[u]));
-        }
-        var json = new JavaScriptSerializer().Serialize(responseList);
-        */
-        for (int u = 0; u < V; u++) result[u]++;
-        string sudokuSolved = string.Join("", result);
-        return sudokuSolved;
-    }
-
-    private void AsignColors(bool[] available)
-    {
-        for (int u = 0; u < V; u++)
-        {
-            /*Console.WriteLine("\n---------------");*/
-            /* for (int k = 0; k < V; k++) Console.Write(" " + result[k]);*/
-            // Process all adjacent vertices and flag their colors
-            // as unavailable
-            IEnumerator<int> it = adj[u].GetEnumerator();
-            while (it.MoveNext())
-            {
-                int i = it.Current;
-                if (result[i] != -1)
-                {
-                    available[result[i]] = true;
-                }
-            }
-            /*Console.WriteLine("\n");
-            for (int k = 0; k < 10; k++) Console.Write(" " + available[k]);*/
-
-            // Find the first available color
-            int false_amount = available.Count(cr => cr == false);
-            
-            if(false_amount==1){
-                int cr;
-                for (cr = 0; cr < V; cr++)
-                {
-                    if (available[cr] == false)
-                    {
-                        break;
-                    }
-                }
-                Console.WriteLine("case "+u+" mise à "+cr);
-                result[u] = cr; // Assign the found color
-            } else {
-                /*Console.WriteLine("raté : "+u+" car couleurs "+false_amount+" possibles");*/
-            }
-                
-            // Reset the values back to false for the next iteration
-            it = adj[u].GetEnumerator();
-            while (it.MoveNext())
-            {
-                int i = it.Current;
-                if (result[i] != -1)
-                {
-                    available[result[i]] = false;
-                }
-            }
-        }
-    }
-
-    private bool[] StoreColors()
-    {
-        bool[] available = new bool[9];
-        for (int cr = 0; cr < 9; cr++)
-        {
-            available[cr] = false;
-        }
-
-        return available;
-    }
-
-}
-
-
-
 
 class DSatur
 {
@@ -293,7 +133,6 @@ class DSatur
         RecursiveSolve(dataMatrix, 0);
         string sudokuSolved = "";
         for (int i = 0; i < v; i++) sudokuSolved+= dataMatrix[1, i];
-        // Console.WriteLine("c");
         return sudokuSolved;
     }
     public void RecursiveSolve(int[,] dataMatrix, int previous_index)
@@ -307,7 +146,6 @@ class DSatur
             }
             if(zero_count == 0){
                 solved = true;
-                Console.WriteLine("C'EST FINI");
                 return;
             }
 
@@ -335,27 +173,16 @@ class DSatur
             {
                 if(dataMatrix[1, w] != 0){
                     colorSetIndex.Add(dataMatrix[1, w]);
-                    // Console.WriteLine(index);
                 }
             }
 
             // Check if there is no available colors at all
-            // Console.WriteLine("\n");
-            // Console.WriteLine("indice actuel : "+index);
-            // Console.WriteLine("indice précedent : "+previous_index);
-            // Console.WriteLine("Couleur à l'indice précédent :"+dataMatrix[1,previous_index]);
-            // Console.WriteLine("indice : "+index);
-            // Console.WriteLine("ColorCount :"+colorSetIndex.Count);
             if(colorSetIndex.Count == 9){
                 dataMatrix[1,previous_index] = 0;
                 for (int i = 0; i < dataMatrix.GetLength(1); i++)
                  {
                      dataMatrix[2, i] = 0;
                  }
-                // Console.WriteLine("\n");
-                // Console.WriteLine("Indice remis à 0 : "+previous_index);
-                // Console.WriteLine(dataMatrix[1,previous_index]);
-                // Console.WriteLine("a");
                 return;
             } 
             else 
@@ -363,23 +190,18 @@ class DSatur
                   for(int i = 1; i < 10 ;i++){
                       if(!colorSetIndex.Contains(i) && solved == false){
                          dataMatrix[1,index] = i;
-                        //  Console.WriteLine("couleur :"+i);
-                          // Console.WriteLine("case changée en "+dataMatrix[1,index]);
-                          // Console.WriteLine("\n");
                         for (int j = 0; j < dataMatrix.GetLength(1); j++)
                         {
                             dataMatrix[2, j] = 0;
                         }
                         RecursiveSolve(dataMatrix,index);
                     }
-                    // else {Console.WriteLine("d");}
                         
                 }
 
                 if(solved == false){
                  dataMatrix[1,index] = 0;
                 }
-                // Console.WriteLine("e");
             }
 
         
