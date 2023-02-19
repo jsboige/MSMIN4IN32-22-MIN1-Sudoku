bba0a1fe785cc01181feeb984cb885d1cec988ff
chambellan <100809259+chambellan@users.noreply.github.com>
2023-02-20 00:36:41 +0100

    Ajout du modèle entraîné sur python

---
+++ Sudoku.CNN/SolverKeras_CNN.cs
@@ -0,0 +1,21 @@
+﻿using Keras.Models;
+using Sudoku.Shared;
+
+
+namespace Sudoku.CNN
+{
+    public class SolverKeras_CNN : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            var model = Model.LoadModel('saved_model.pb');
+            return smart_solve(s, model);
+        }
+
+        private SudokuGrid smart_solve(SudokuGrid s, Keras.Models.BaseModel model)
+        {
+            
+            
+        }
+    }
+}
