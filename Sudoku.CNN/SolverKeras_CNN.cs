053bee8dea4ca6555483c759a7a4bf957a7fc54c
chambellan <100809259+chambellan@users.noreply.github.com>
2023-02-20 20:08:14 +0100

    Implémentation solver simple

--- Sudoku.CNN/SolverKeras_CNN.cs
+++ Sudoku.CNN/SolverKeras_CNN.cs
@@ -1,4 +1,5 @@
 ﻿using Keras.Applications;
+using Numpy;
 using Sudoku.Shared;
 using System.Diagnostics;
 
@@ -15,7 +16,31 @@ namespace Sudoku.CNN
 
         private SudokuGrid Simple_solver(SudokuGrid s, Keras.Models.BaseModel model)
         {
+            //Conversion de la SudokuGrid en array Numpy pour l'injection dans le CNN
+            int[][] SudokuBuffer = new int[9][];
+            for (int i = 0;i < 9; i++)
+            {
+                for (int j = 0; j<9; j++)
+                {
+                    SudokuBuffer[i][j] = s.Cells[i][j];
+                }
+            }
+
+            //Prediction du CNN
+            var prediction = np.array(SudokuBuffer);
+            model.Predict(prediction);
+            SudokuGrid sol = new SudokuGrid();
             
+            //Reconversion de la solution en SudokuGrid
+            for(int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j<9; j++)
+                {
+                    sol.Cells[i][j] = (int)prediction[i][j];
+                }
+            }
+
+            return sol;
         }
 
         
