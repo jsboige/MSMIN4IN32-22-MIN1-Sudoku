d760b6c53ae12fdfaf053bfa7a77c798ec7299b1
chambellan <100809259+chambellan@users.noreply.github.com>
2023-02-20 19:32:59 +0100

    Simplification solver

--- Sudoku.CNN/SolverKeras_CNN.cs
+++ Sudoku.CNN/SolverKeras_CNN.cs
@@ -1,6 +1,6 @@
-﻿using Keras.Models;
+﻿using Keras.Applications;
 using Sudoku.Shared;
-
+using System.Diagnostics;
 
 namespace Sudoku.CNN
 {
@@ -8,73 +8,16 @@ namespace Sudoku.CNN
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
-            var model = Model.LoadModel('saved_model.pb');
-            return smart_solve(s, model);
+            string filepath = "saved_model.pb";
+            Keras.Models.BaseModel model = Keras.Models.BaseModel.LoadModel(filepath);
+            return Simple_solver(s, model);
         }
 
-        private SudokuGrid smart_solve(SudokuGrid s, Keras.Models.BaseModel model)
+        private SudokuGrid Simple_solver(SudokuGrid s, Keras.Models.BaseModel model)
         {
-            using System;
-            using System.Linq;
-            using System.Numerics;
-
-            public static int[][][] BatchSmartSolve(int[][][] grids, Model solver)
-            {
-                int[][][] result = new int[grids.Length][][];
-                grids.CopyTo(result, 0);
-
-                int numBlanks = grids.Select(g => g.SelectMany(row => row).Count(x => x == 0)).Max();
-
-                for (int i = 0; i < numBlanks; i++)
-                {
-                    float[][][] preds = solver.Predict(ToCategorical(result));
-                    float[][] probs = preds.Select(p => p.Select(row => row.Max()).ToArray()).ToArray();
-                    int[][] values = preds.Select(p => p.Select(row => Array.IndexOf(row, row.Max()) + 1).ToArray()).ToArray();
-                    bool[][] blanks = result.Select(g => g.SelectMany(row => row).Select(x => x == 0).ToArray()).ToArray();
-
-                    for (int j = 0; j < result.Length; j++)
-                    {
-                        if (blanks[j].Any(b => b))
-                        {
-                            int[] blankIndices = blanks[j].Select((b, k) => new { b, k }).Where(x => x.b).Select(x => x.k).ToArray();
-                            int[] confidences = blankIndices.Select(k => probs[j][k] == 0 ? -1 : Array.IndexOf(preds[j][k], preds[j][k].Max())).ToArray();
-                            int[] bestConfidences = blankIndices.Select(k => confidences[k] == -1 ? -1 : confidences.Where(c => c >= 0).OrderByDescending(c => probs[j][k]).First()).ToArray();
-                            int[] bestValues = blankIndices.Select(k => bestConfidences[k] == -1 ? 0 : values[j][k][bestConfidences[k]]).ToArray();
-
-                            for (int k = 0; k < blankIndices.Length; k++)
-                            {
-                                if (bestConfidences[k] >= 0)
-                                {
-                                    int[] fillIndices = new int[] { blankIndices[k] % 9, blankIndices[k] / 9 };
-                                    result[j][fillIndices[1]][fillIndices[0]] = bestValues[k];
-                                }
-                            }
-                        }
-                    }
-                }
-
-                return result;
-            }
-
-            private static float[][][] ToCategorical(int[][][] grids)
-            {
-                float[][][] result = new float[grids.Length][][];
-                for (int i = 0; i < grids.Length; i++)
-                {
-                    result[i] = new float[9][];
-                    for (int j = 0; j < 9; j++)
-                    {
-                        result[i][j] = new float[81];
-                        for (int k = 0; k < 81; k++)
-                        {
-                            result[i][j][k] = grids[i][k / 9][k % 9] == j + 1 ? 1 : 0;
-                        }
-                    }
-                }
-                return result;
-            }
-
-
+            
         }
+
+        
     }
 }
