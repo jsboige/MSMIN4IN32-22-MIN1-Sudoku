e9267554d4007a06f5a911201296cba748f8cd98
chambellan <100809259+chambellan@users.noreply.github.com>
2023-02-21 19:00:51 +0100

    Conversion du modèle en format .h5

--- Sudoku.CNN/SolverKeras_CNN.cs
+++ Sudoku.CNN/SolverKeras_CNN.cs
@@ -22,29 +22,33 @@ namespace Sudoku.CNN
 
 
 
-			string filepath = Path.Combine(Environment.CurrentDirectory, "saved_model.pb");
-            if (!File.Exists(filepath))
-            {
-				File.WriteAllBytes(filepath, Resource1.saved_model);
-			}
+            //string filepath = Path.Combine(Environment.CurrentDirectory, "variables");
+            string filepath = "C:\\Users\\lolfi\\Documents\\GitHub\\MSMIN4IN32-22-MIN1-Sudoku\\Sudoku.CNN\\New_Model.h5";
+            //if (!File.Exists(filepath))
+            //{
+				//File.WriteAllBytes(filepath, Resource1.variables);
+			//}
 			
-			Keras.Models.BaseModel model = Keras.Models.BaseModel.LoadModel(filepath);
+			Keras.Models.BaseModel model = Keras.Models.Model.LoadModel(filepath);
             return Simple_solver(s, model);
         }
 
         private SudokuGrid Simple_solver(SudokuGrid s, Keras.Models.BaseModel model)
         {
             //Conversion de la SudokuGrid en array Numpy pour l'injection dans le CNN
-            int[][] SudokuBuffer = new int[9][];
-            for (int i = 0;i < 9; i++)
+            int[,] SudokuBuffer = new int[9,9];
+            
+            for (int i=0; i<9; i++)
             {
-                for (int j = 0; j<9; j++)
+                for (int j = 0; j < 9; j++)
                 {
-                    SudokuBuffer[i][j] = s.Cells[i][j];
+                    SudokuBuffer[i, j] = s.Cells[i][j];
                 }
-            }
+            } 
+            
 
             //Prediction du CNN
+            
             var prediction = np.array(SudokuBuffer);
             model.Predict(prediction);
             SudokuGrid sol = new SudokuGrid();
