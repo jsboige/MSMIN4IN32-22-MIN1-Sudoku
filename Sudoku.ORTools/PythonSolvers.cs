1daa78cc548ddfb4b8853e6b068169aa32ac6260
BADOUANA <badcedric25@gmail.com>
2023-02-21 15:10:59 +0100

    Solver_Lineaire_Implemente

--- Sudoku.ORTools/PythonSolvers.cs
+++ Sudoku.ORTools/PythonSolvers.cs
@@ -47,4 +47,47 @@ namespace Sudoku.ORTools
             base.InitializePythonComponents();
         }
     }
+
+
+
+
+public class OrToolsPythonSolverLinear : PythonSolverBase
+    {
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+            //System.Diagnostics.Debugger.Break();
+            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+            //using (Py.GIL())
+            //{
+            // create a Python scopes
+            using (PyModule scope = Py.CreateScope())
+            {
+
+				var strSudoku = s.Cells.SelectMany(row => row.Select(c => c.ToString(CultureInfo.InvariantCulture))).Aggregate("", (current, c) => current + c);
+
+				// convert the Person object to a PyObject
+				PyObject pySudoku = strSudoku.ToPython();
+                // create a Python variable "person"
+                scope.Set("instance", pySudoku);
+				
+				AddNumpyConverterScript(scope);
+                // the person object may now be used in Python
+                string code = Resources.PythonOrtoolsSolverLinear_Py;
+                scope.Exec(code);
+                var result = scope.Get("r");
+                var managedResult = result.As<int[,]>();
+                var managedJaggedArray = managedResult.ToJaggedArray();
+                return new Shared.SudokuGrid() { Cells = managedJaggedArray };
+        
+            }
+            //}
+        }
+        
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("ortools");
+            base.InitializePythonComponents();
+        }
+    }
+
     }
